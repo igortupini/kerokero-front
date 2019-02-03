86a3f7e34b7a8fae6acd1f03163cd739d8cc141a
Tupini <piradinho1@gmail.com>
2019-02-03 03:17:15 -0800

    Add fetch kweets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 //Components
 import Menu from './Menu'
 import KweetList from './KweetList'
 
+//Configs
+const API_URL = 'http://192.168.1.100:3000/api'
+
 class App extends Component {
-  render() {
-	return (
-		<div>
-			<Menu />
-			<KweetList />
-		</div>
-	);
-  }
+
+	constructor(props){
+		super(props)
+		this.state = {
+			kweets: []
+		}
+	}
+
+	fetchKweets(){
+		return axios.get(API_URL+'/kweet')
+		.then(res => {
+			let kweets = res.data
+			this.setState({kweets})
+		})
+		.catch(e => console.error(e))
+	}
+
+	componentDidMount(){
+		return this.fetchKweets()
+	}
+	
+
+	render() {
+		let { kweets } = this.state
+		console.log(kweets)
+		return (
+			<div>
+				<Menu />
+				<KweetList kweets={kweets} />
+			</div>
+		);
+  	}
 }
 
 export default App
--- src/KweetList.js
+++ src/KweetList.js
@@ -3,14 +3,25 @@ import styles from './KweetList.module.css'
 
 const KweetList = props => {
 return (
-<div className={styles.nav}>
-<div className={styles.navItems}>
-    <ul>
-        <li>Home</li>
-        <li>Logout</li>
-    </ul>
-</div>
-</div>
+    <div className={styles.container}>
+        <div className={styles.list}>
+        <small>{props.kweets.length&&props.kweets[0].content}</small>
+            {props.kweets.map(kweet => {
+                return (
+                    <div className={styles.kweetItem}>
+                        <div><b>First Name</b> - 4 min</div>
+                        <p>{kweet.content}</p>
+                        <div className={styles.kweetInfo}>
+                            <span><i className="material-icons">thumb_up</i></span>
+                            <span><i className="material-icons">refresh</i></span>
+                            <span><i className="material-icons">add_alert</i></span>
+                        </div>
+                    </div>
+                )
+            })
+            }
+        </div>
+    </div>
 )}
 
 export default KweetList
